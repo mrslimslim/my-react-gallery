4599c58edf8bd8c7d6b154d89f4960add4d61872
mrslimslim <2451319596@qq.com>
2020-03-02 21:26:08 +0800

    add: sass

---
+++ application/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      app start
+    </div>
+  );
+}
+
+export default App;
